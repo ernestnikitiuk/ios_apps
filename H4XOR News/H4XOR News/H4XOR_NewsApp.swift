424bde4dcb61a3f2e86d82abeb74705738595cd5
ernestnikitiuk <48037146+ernestnikitiuk@users.noreply.github.com>
2023-12-03 13:25:45 +0300

    Add files via upload

---
+++ H4XOR News/H4XOR News/H4XOR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_NewsApp.swift
+//  H4XOR News
+//
+//  Created by Erik on 27.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ H4XOR News/H4XOR News/Views/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by Erik on 27.10.2023.
+//
+
+import SwiftUI
+
+
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView{
+            List(networkManager.posts) { post in
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack{
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                }
+                
+            }.listStyle(.plain)
+                .navigationTitle("H4XOR news")
+        }
+        .onAppear {
+            networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+//struct Post: Identifiable {
+//    let id: String
+//    let title: String
+//}
+//
+//let posts = [
+//    Post(id: "1", title: "Hello"),
+//    Post(id: "2", title: "Bonjour"),
+//    Post(id: "3", title: "Holla")
+//]
